2a3cede95ac68d9829ba5e352adf78e3a219f197
YiZhiYuanYuan-qyy <73566575+YiZhiYuanYuan-qyy@users.noreply.github.com>
2021-10-02 12:49:59 +0800

    Delete lab1 directory

--- lab1/problem1/problem1.m
+++
@@ -1,8 +0,0 @@
-function [an] = problem1(N,l,a)
-% N is the number of trials
-% l is the length of needles
-% a is the length of the parallel lines
-d = 0.5*rand(1,N);
-theta = rand(1,N)*0.5*pi;
-k = sum(d<0.5*l*sin(theta));
-an = (2*N*l)/(k*a);
